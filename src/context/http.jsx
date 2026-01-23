c9e5d2a1c55555275d27679396298de3ee41bb2a
roeungnakproject <roeungnak.project@gmail.com>
2026-01-24 01:19:02 +0700

    Ecommerce-Frontend

--- src/context/http.jsx
+++ src/context/http.jsx
@@ -1,4 +1,5 @@
-export const apiUrl = "https://laravel-production-aca7.up.railway.app/api";
+export const apiUrl =
+  "https://ecommerce-backend-production-4791.up.railway.app/api";
 export const adminToken = () => {
   const data = JSON.parse(localStorage.getItem("adminInfo"));
   return data.token;
